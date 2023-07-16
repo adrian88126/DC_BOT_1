743dfa9a11065c8565d005618d4bbe85210a9ed9
adrian88126 <a16test1@gmail.com>
2023-07-16 17:12:06 +0800

    Update handle_message.py

--- handle_message.py
+++ handle_message.py
@@ -1,6 +1,6 @@
 async def handle_message(message):
-    if message.content == '我好帥喔':
+    if message.content == '我好帥':
         # 刪除傳送者的訊息
         await message.delete()
         # 然後回傳訊息
-        await message.channel.send('不好意思，不要騙人啦')
+        await message.channel.send('不要騙人')
