05af45568bb779902ccc82b1d1a2dd0f4a619a07
adrian88126 <a16test1@gmail.com>
2023-07-16 13:01:05 +0800

    Add .gitignore file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from discord.ext import commands
 intents = discord.Intents.default()
 intents.typing = False
@@ -7,4 +8,4 @@ bot = commands.Bot(command_prefix="[", intents=intents)
 @bot.event
 async def on_ready():
     print(">>bot online>>")
-bot.run('MTEyOTc3NTI2NTI0NjA4OTM4OA.GWPxZf.cEBML0MW9jqgsWmjzvZkTAgFQTw5Q-UoGQqtL4')
+bot.run(os.environ['BOT_TOKEN'])
