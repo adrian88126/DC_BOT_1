bc846a5d7de4e86e47f94fdb54654ab46076a0f2
adrian88126 <a16test1@gmail.com>
2023-07-16 15:46:04 +0800

    Update member_handler.py

--- member_handler.py
+++ member_handler.py
@@ -1,6 +1,10 @@
-def handle_member_join(member):
+async def handle_member_join(member, channel):
     # 在這裡進行成員加入的處理
     print(f"{member.name} 加入了伺服器")
-def handle_member_remove(member):
+    # 在指定的頻道中發送訊息
+    await channel.send(f"{member.name} 加入了伺服器！歡迎！")
+async def handle_member_remove(member, channel):
     # 在這裡進行成員退出的處理
     print(f"{member.name} 退出了伺服器")
+    # 在指定的頻道中發送訊息
+    await channel.send(f"{member.name} 退出了伺服器！掰掰！")
