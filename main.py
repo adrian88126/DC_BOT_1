86ba080b505494c5ccacece001865109d63d6954
adrian88126 <a16test1@gmail.com>
2023-07-16 12:25:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import discord
 from discord.ext import commands
-bot = commands, bot(command_prePix="[")
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+bot = commands.Bot(command_prefix="[", intents=intents)
 @bot.event
-async def on_readry():
+async def on_ready():
     print(">>bot online>>")
+bot.run('MTEyOTc3NTI2NTI0NjA4OTM4OA.GWPxZf.cEBML0MW9jqgsWmjzvZkTAgFQTw5Q-UoGQqtL4')
